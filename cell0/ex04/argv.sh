8cdbe957935374a9c1a84b2cc1bc05af1d6100b9
Mexil77 <mendel_77@hotmail.com>
2022-02-16 14:02:58 +0100

    subida

---
+++ cell0/ex04/argv.sh
@@ -0,0 +1,11 @@
+if [ "$1" != "" ]; then
+	echo $1
+else
+	echo "No arguments supplied"
+fi
+if [ "$2" != "" ]; then
+	echo $2
+fi
+if [ "$3" != "" ]; then
+	echo $3
+fi
---
+++ cell0/ex05/build.sh
@@ -0,0 +1,8 @@
+if [ "$1" == "" ]; then
+	echo "No arguments supplied"
+else
+	for i in $*
+	do
+	mkdir "ex$i"
+	done
+fi
